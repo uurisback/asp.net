36b646db632292169f1e7f32f9aa86af60e85292
uurisback <101743299+uurisback@users.noreply.github.com>
2022-03-17 19:15:15 +0300

    asp.net proje

---
+++ Models/odevM.cs
@@ -0,0 +1,11 @@
+
+    public class odevM
+    {
+        public string ad{get;set;}
+        public  string soyad{get;set;}
+        public bool telefonNumarası{get;set;}
+        public double telefon{get;set;}
+    public bool ehliyetDurumu{get;set;}
+    public int yasiniz{get;set;}
+
+    }
